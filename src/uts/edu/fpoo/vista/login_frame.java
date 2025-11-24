6fda509a5a38932ae4b2c3dbd2efe8673fbc7c12
sebastian <sebasr1268@gmail.com>
2025-11-23 19:38:09 -0500

    arreglando bugs sobre el menu de opciones

--- src/uts/edu/fpoo/vista/login_frame.java
+++ src/uts/edu/fpoo/vista/login_frame.java
@@ -235,6 +235,7 @@ public class login_frame extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(MenuOpciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
